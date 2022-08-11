11fbc3f046ade2410625b0f45ea18690fa0e8f79
Asmodeus9563 <wjoe1995@live.com>
2022-08-11 10:32:02 -0600

    comit

---
+++ analizador_lexico.py
@@ -0,0 +1,121 @@
+import ply.lex as lex
+
+# resultado del analisis
+resultado_lexema = []
+
+reservada = (
+    # Palabras Reservadas
+    'MOSTRAR',  #imprimir en pantalla
+    'FOR',      #repetir una accion varias veces
+    'CONCAT',   #concatenar dos cadenas
+    'CADENA',   #cadena de texto
+    
+)
+tokens = reservada + (
+    'IDENTIFICADOR',    #identificador (nombre de variable, nombre de funcion, etc)
+    'ENTERO',        #numero decimal (1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0)
+    'ASIGNAR',          #asignacion de valor a=5
+    'SUMA',             #suma 25+30=55
+    'RESTA',            #resta 25-5=20
+    'MULT',             #multiplicacion 5*5=25
+    'DIV',              #division 10/2 = 5
+    'POTENCIA',         #potencia 6**2 = 6^2 = 6*6 = 36
+    'MODULO',           #modulo 10%2 = 0
+    'PARIZQ',            #simbolo (
+    'PARDER',            #simbolo )
+    'CORIZQ',            #simbolo [
+    'CORDER',            #simbolo ]
+    'LLAIZQ',            #simbolo {
+    'LLADER',            #simbolo }
+    'COMDOB'             #simbolo "
+)
+
+# Reglas de Expresiones Regualres para token de Contexto simple
+t_SUMA          = r'\+'
+t_RESTA         = r'-'
+t_COMDOB        = r'\"'
+t_MULT          = r'\*'
+t_DIV           = r'/'
+t_MODULO        = r'\%'
+t_POTENCIA      = r'(\*{2} | \^)'
+t_ASIGNAR       = r'='
+t_PARIZQ        = r'\('
+t_PARDER        = r'\)'
+t_CORIZQ        = r'\['
+t_CORDER        = r'\]'
+t_LLAIZQ        = r'{'
+t_LLADER        = r'}'
+
+def t_FOR(t):
+    r'for'
+    return t
+
+def t_MOSTRAR(t):
+    r'mostrar'
+    return t
+
+def t_CONCAT(t):
+    r'concat'
+    return t
+
+def t_ENTERO(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+def t_CADENA(t):
+    r'\"?(\w+ \ *\w*\d* \ *)\"?'
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_comments(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+    print("Comentario de multiple linea")
+
+def t_comments_ONELine(t):
+    r'\/\/(.)*\n'
+    t.lexer.lineno += 1
+    print("Comentario de una linea")
+t_ignore =' \t'
+
+def t_error( t):
+    global resultado_lexema
+    estado = "** Token no valido en la Linea {:4} Valor {:16} Posicion {:4}".format(str(t.lineno), str(t.value),
+                                                        str(t.lexpos))
+    resultado_lexema.append(estado)
+    t.lexer.skip(1)
+
+# Prueba de ingreso
+def prueba(data):
+    global resultado_lexema
+
+    analizador = lex.lex()
+    analizador.input(data)
+
+    resultado_lexema.clear()
+    while True:
+        tok = analizador.token()
+        if not tok:
+            break
+        # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
+        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
+        resultado_lexema.append(estado)
+    return resultado_lexema
+
+# instanciamos el analizador lexico
+analizador = lex.lex()
+
+'''if __name__ == '__main__':
+    while True:
+        data = input("ingrese: ")
+        prueba(data)
+        print(resultado_lexema)'''
+        
---
+++ analizador_sintactico.py
@@ -0,0 +1,137 @@
+import ply.yacc as yacc
+from analizador_lexico import tokens
+from analizador_lexico import analizador
+
+# resultado del analisis
+resultado_gramatica = []
+
+precedence = (
+    ('right','ASIGNAR'),
+    ('left', 'SUMA', 'RESTA'),
+    ('left', 'MULT', 'DIV'),
+    ('right', 'CONCAT', 'MOSTRAR',),
+    ('left', 'FOR',),
+)
+nombres = {}
+
+def p_declaracion_mosrar(t):
+    '''
+    declaracion :    MOSTRAR expresion
+                    |MOSTRAR CADENA
+    '''
+    
+    t[0] = print(t[2])
+
+def p_expresion_for(t):
+    'expresion : ENTERO  FOR  CADENA'
+    t[0] = t[1]
+    for i in range(t[1]):
+        print(t[3])
+
+def p_declaracion_concat(t):
+    'declaracion : CADENA CONCAT CADENA'
+    t[0] = t[1] + t[3]
+    
+
+def p_declaracion_asignar(t):
+    '''
+    declaracion :  IDENTIFICADOR ASIGNAR expresion
+                |  IDENTIFICADOR ASIGNAR CADENA
+    '''
+    if len(t) == 4:
+        nombres[t[1]] = t[3]
+    else:
+        nombres[t[1]] = t[5]
+
+def p_declaracion_expre(t):
+    'declaracion : expresion'
+    # print("Resultado: " + str(t[1]))
+    t[0] = t[1]
+
+def p_expresion_operaciones(t):
+    '''
+    expresion  :   expresion SUMA expresion
+                |   expresion RESTA expresion
+                |   expresion MULT expresion
+                |   expresion DIV expresion
+                |   expresion POTENCIA expresion
+                |   expresion MODULO expresion
+    '''
+    if t[2] == '+':
+        t[0] = t[1] + t[3]
+    elif t[2] == '-':
+        t[0] = t[1] - t[3]
+    elif t[2] == '*':
+        t[0] = t[1] * t[3]
+    elif t[2] == '/':
+        t[0] = t[1] / t[3]
+    elif t[2] == '%':
+        t[0] = t[1] % t[3]
+    elif t[2] == '**':
+        i = t[3]
+        t[0] = t[1]
+        while i > 1:
+            t[0] *= t[1]
+            i -= 1
+
+def p_expresion_grupo(t):
+    '''
+    expresion  :  PARIZQ expresion PARDER
+                | LLAIZQ expresion LLADER
+                | CORIZQ expresion CORDER
+    '''
+    t[0] = t[2]
+
+def p_expresion_numero(t):
+    '''
+    expresion : ENTERO
+    '''
+    t[0] = t[1]
+
+def p_declaracion_cadena(t):
+    'declaracion : COMDOB expresion COMDOB'
+    t[0] = t[2]
+
+def p_expresion_nombre(t):
+    'expresion : IDENTIFICADOR'
+    try:
+        t[0] = nombres[t[1]]
+    except LookupError:
+        print("Nombre desconocido ", t[1])
+        t[0] = 0
+
+def p_error(t):
+    global resultado_gramatica
+    if t:
+        resultado = "Error sintactico de tipo {} en el valor {}".format( str(t.type),str(t.value))
+        print(resultado)
+    else:
+        resultado = "Error sintactico {}".format(t)
+        print(resultado)
+    resultado_gramatica.append(resultado)
+
+# instanciamos el analizador sistactico
+parser = yacc.yacc()
+
+def prueba_sintactica(data):
+    global resultado_gramatica
+    resultado_gramatica.clear()
+    for item in data.splitlines():
+        if item:
+            gram = parser.parse(item)
+            if gram:
+                resultado_gramatica.append(str(gram))
+        else: print("data vacia")
+    print("result: ", resultado_gramatica)
+    return resultado_gramatica
+
+if __name__ == '__main__':
+    while True:
+        try:
+            s = input(' ingresa dato >>: ')
+        except EOFError:
+            continue
+        if not s: continue
+        #gram = parser.parse(s)
+        #print("Resultado ", gram)
+        prueba_sintactica(s)
